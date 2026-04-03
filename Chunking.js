b9be586e90caa2f35618a7419029b8d1cdeb185e
Alfred01-HUB <alfredolabanji066@gmail.com>
2026-04-03 23:57:10 +0100

    feat: implement Chunking for load control

---
+++ Chunking.js
@@ -0,0 +1,50 @@
+async function runWithLimit(tasks, limit = 3) {
+  const results = [];
+  let index = 0;
+
+  async function worker() {
+    while (index < tasks.length) {
+      const currentIndex = index++;
+      const task = tasks[currentIndex];
+
+      try {
+        const result = await task();
+        results[currentIndex] = result;
+        console.log(` Done task ${currentIndex}`);
+      } catch (err) {
+        console.log(` Failed task ${currentIndex}`);
+      }
+    }
+  }
+
+  // create workers
+  const workers = Array.from({ length: limit }, () => worker());
+
+  await Promise.all(workers);
+
+  return results;
+}
+
+
+const tasks = Array.from({ length: 6 }, (_, i) => {
+  return async () => {
+    console.log(`⏳ Start task ${i}`);
+
+    // simulate delay (2 seconds)
+    await new Promise(res => setTimeout(res, 2000));
+
+    console.log(` End task ${i}`);
+    return i;
+  };
+});
+
+(async () => {
+  const start = Date.now();
+
+  const results = await runWithLimit(tasks, 2);
+
+  const end = Date.now();
+
+  console.log("\nResults:", results);
+  console.log("Total time:", end - start, "ms");
+})();
